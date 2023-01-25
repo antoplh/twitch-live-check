e43c7d06be2b5e78b53908dbead0e1c39daefcfb
Antonella Portugal <81722618+antoplh@users.noreply.github.com>
2023-01-25 00:12:30 -0500

    Update twitch-live.py

--- twitch-live.py
+++ twitch-live.py
@@ -3,6 +3,22 @@ import requests
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    html = """
+    <html>
+        <body>
+            <h1>Welcome to the Livestreamer API</h1>
+            <form action="{{ url_for('is_live', channel_name=channel_name) }}" method="get">
+                <label>Enter the channel name you want to check:</label>
+                <input type="text" name="channel_name">
+                <input type="submit" value="Submit">
+            </form>
+        </body>
+    </html>
+    """
+    return make_response(html)
+
 @app.route('/<channel_name>', methods=['GET'])
 def is_live(channel_name):
     response = requests.get(f"https://www.twitch.tv/{channel_name}")
